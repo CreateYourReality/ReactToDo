65847c1f692096273e0366502937772d7defed14
CreateYourReality <timo.moewes@icloud.com>
2023-06-15 12:32:32 +0200

    final

---
+++ react-todo/src/pages/Home.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+const Home = () => {
+    const [data,setData] = useState([]);
+    const [input, setInput] = useState("");
+    const [checked, setChecked] = useState([]);
+
+    const addToDo = (event) => {
+        event.preventDefault();
+        setData([...data, input]);
+        setChecked([...checked,false]);
+    }
+    
+    const handleInput = (event) => {
+        setInput(event.target.value);
+    }
+
+    const deleteMe = (index) => {
+        let newArray = [...data];
+        newArray.splice(index,1);
+        setData(newArray);
+    }
+
+    const handleVisuals = (index) => {
+        let newChecked = [...checked];
+        newChecked[index] = !newChecked[index];
+        setChecked(newChecked);
+    }
+
+    useEffect(() => {
+        console.log("NEW");
+    }, [data])
+
+    return ( 
+        <section>
+            <nav>
+                <h1>TODO</h1>
+            </nav>
+
+            <article>
+                {data.map((item,index) => (
+                    <div key={index} className="todo-item">
+                        <input onChange={() => handleVisuals(index)} type="checkbox" />
+                        <p className={checked[index]?"visuals":""}>{item}</p>
+                        <a onClick={() => deleteMe(index)}>DELETE</a>
+                    </div>
+                ))
+                }
+            </article>
+
+            <form >
+                <input onChange={handleInput} type="text" />
+                <input onClick={addToDo} type="submit" value="Submit"/>
+            </form>
+            
+        </section>
+     );
+}
+ 
+export default Home;
